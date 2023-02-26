79bb30fe2f40d5184fdecd22b56d6299af3edc28
Phan Ngọc Cường <cuongharryit@gmail.com>
2023-02-26 23:53:59 +0700

    responsive diem in mobile

---
+++ js/xuLiClick_Diem.js
@@ -0,0 +1,5 @@
+function showTable__mini() {
+    document.getElementsByClassName('table-1-diem-tat-ca')[0].style.display = 'block';
+    document.getElementsByClassName('table-2-diem-trung-binh')[0].style.display = 'none';
+    document.getElementsByClassName('table-2-diem-trung-binh__mini')[0].style.display = 'none';
+}
